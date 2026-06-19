51cc4a6b81f1df4109ec25fe36939d1a531dfd95
Jeremy Giuliana <jeremy.giuliana04@gmail.com>
2026-06-19 20:41:33 +0200

    Correction redirection email

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -34,6 +34,9 @@ function Auth() {
     const { error } = await supabase.auth.signUp({
       email,
       password,
+      options: {
+        emailRedirectTo: window.location.origin
+      } 
     })
 
     if (error) {
